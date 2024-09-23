94c7773813221072143de98283ebcbdb03efcf40
Kashif Ahmad <19bcs2983@gmail.com>
2024-09-23 18:58:45 +0530

    changes

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://carservice-backend.vercel.app//api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
