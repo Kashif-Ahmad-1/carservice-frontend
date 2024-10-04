cecd2573f761789b3f6b244eab7eee27403bf5db
Kashif Ahmad <19bcs2983@gmail.com>
2024-10-04 20:39:27 +0530

    toastify error

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const API_BASE_URL = 'http://localhost:5000';
 // const API_BASE_URL = 'https://backend-2-amber.vercel.app';
 
 const WHATSAPP_CONFIG = {
-  url: 'http://localhost:8080/https://app.messageautosender.com/api/v1/message/create',
+  url: 'https://cors-anywhere.herokuapp.com/https://app.messageautosender.com/api/v1/message/create',
   username: 'kashif3789',
   password: 'test@123',
 };
