e45331e480e3eb8987a3fd26f3c114f4fc6e1429
Kashif Ahmad <19bcs2983@gmail.com>
2024-10-10 23:02:50 +0530

    build added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-const API_BASE_URL = 'http://localhost:5000';
-// const API_BASE_URL = 'https://backend-2-amber.vercel.app';
+// const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://backend-2-amber.vercel.app';
 
 const WHATSAPP_CONFIG = {
   url: 'https://cors-anywhere.herokuapp.com/https://app.messageautosender.com/api/v1/message/create',
